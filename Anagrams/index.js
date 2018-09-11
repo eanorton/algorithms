0bf22fae367edccf3c174c7952d38caa7ce10e7f
eanorton <edwardnorton@icloud.com>
2018-09-11 14:57:09 -0400

    obj map for str in anagrams

--- Anagrams/index.js
+++ Anagrams/index.js
@@ -15,3 +15,33 @@ function anagrams(stringA, stringB) {
 function cleanString(str) {
   return str.replace(/[^\w]/g, "").toLowerCase().split("").sort().join("")
 }
+
+
+// CREATE OBJECT MAPS FOR STRING:
+
+// function anagrams(stringA, stringB) {
+//   const aCharMap = buildCharMap(stringA);
+//   const bCharMap = buildCharMap(stringB);
+//
+//   if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+//     return false;
+//   }
+//
+//   for (let char in aCharMap) {
+//     if (aCharMap[char] !== bCharMap[char]) {
+//       return false;
+//     }
+//   }
+//
+//   return true;
+// }
+//
+// function buildCharMap(str) {
+//   const charMap = {};
+//
+//   for (let char of str.replace(/[^\w]/g, "").toLowerCase()) {
+//     charMap[char] = charMap[char] + 1 || 1;
+//   }
+//
+//   return charMap;
+// }
